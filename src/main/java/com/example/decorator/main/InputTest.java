ea94eef756bdaa20b2e6977c3cd3464e4412ccd6
jan0112d <jan0112d@gmail.com>
2020-06-04 21:56:51 +0800

    decorator

---
+++ src/main/java/com/example/decorator/main/InputTest.java
@@ -0,0 +1,28 @@
+package com.example.decorator.main;
+
+import com.example.decorator.model.LowerCaseInputStream;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author wll
+ * @data 2020/06/04
+ */
+public class InputTest {
+
+    public static void main(String args[]) {
+        int c;
+        try {
+            InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("src/main/resources/static/decorator/test.txt")));
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
